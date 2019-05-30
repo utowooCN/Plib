1bf0694cff3b7727f461fcc4da01a05d30b0d272
utowooCN <51164072+utowooCN@users.noreply.github.com>
2019-05-30 23:17:21 +0800

    Add template and bootstrap
    initial page template and bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/index.html')
+def index_page():
+    return render_template('index.html')
+
+
+@app.route('/base.html')
+def base_page():
+    return render_template('base.html', name='bootstrap')
 
 
 if __name__ == '__main__':
